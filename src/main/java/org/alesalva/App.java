c2454ec91ebc165db4f2ccdbfa0f970263a6a09d
Alejandra Inés Salvadó <salvadoalejandra@gmail.com>
2022-02-03 20:17:09 -0300

    usuario insert info

--- src/main/java/org/alesalva/App.java
+++ src/main/java/org/alesalva/App.java
@@ -1,17 +1,26 @@
 package org.alesalva;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        String reportOrder = "report1";
-        String reportAdjudication = "report2";
-        String reportRegister = "report3";
-        String reportPayment = "report4";
+    public static void main( String[] args ) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos el "lector"
+        System.out.println("Por favor ingrese el nombre del reporte");//Se pide un dato al usuario
+        String reportOrder = br.readLine(); //Se lee el nombre con readLine() que retorna un String con el dato
+        System.out.println("Por favor ingrese el nombre del reporte");//Se pide un dato al usuario
+        String reportAdjudication = br.readLine();
+        System.out.println("Por favor ingrese el nombre del reporte");//Se pide un dato al usuario
+        String reportRegister = br.readLine();
+        System.out.println("Por favor ingrese el nombre del reporte");//Se pide un dato al usuario
+        String reportPayment = br.readLine();
+
         System.out.printf("Usted ha seleccionado los archivos: %s %s %s %s%n", reportOrder, reportAdjudication, reportRegister, reportPayment);
     }
 }
