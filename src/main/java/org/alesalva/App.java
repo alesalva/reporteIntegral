4e812566a7346f1dcc7ecb9985d5a83259544bdf
Alejandra Inés Salvadó <salvadoalejandra@gmail.com>
2022-02-03 18:29:40 -0300

    create variables

--- src/main/java/org/alesalva/App.java
+++ src/main/java/org/alesalva/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String reportOrder = "report1";
+        String reportAdjudication = "report2";
+        String reportRegister = "report3";
+        String reportPayment = "report4";
+        System.out.printf("Usted ha seleccionado los archivos: %s %s %s %s%n", reportOrder, reportAdjudication, reportRegister, reportPayment);
     }
 }
